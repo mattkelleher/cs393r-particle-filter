5fa8c21c47a2d34394614c341795b13d9a94683c
Franck <franck11235@gmail.com>
2021-10-06 18:01:50 -0500

    Added _Distance, GetPredictedPointCloud, and Update functions

--- src/particle_filter/particle_filter.cc
+++ src/particle_filter/particle_filter.cc
@@ -57,6 +57,10 @@ ParticleFilter::ParticleFilter() :
     prev_odom_angle_(0),
     odom_initialized_(false) {}
 
+float Navigation::_Distance(Vector2f p1, Vector2f p2) {
+  return sqrt(pow(p1.x() - p2.x(), 2) + pow(p1.y() - p2.y(), 2));
+}
+
 void ParticleFilter::GetParticles(vector<Particle>* particles) const {
   *particles = particles_;
 }
@@ -70,50 +74,44 @@ void ParticleFilter::GetPredictedPointCloud(const Vector2f& loc,
                                             float angle_max,
                                             vector<Vector2f>* scan_ptr) {
   vector<Vector2f>& scan = *scan_ptr;
+  const float distance_base2lidar = 1; // Measure
+  float phi;
   // Compute what the predicted point cloud would be, if the car was at the pose
   // loc, angle, with the sensor characteristics defined by the provided
   // parameters.
   // This is NOT the motion model predict step: it is the prediction of the
   // expected observations, to be used for the update step.
 
+  float x_base2lidar = distance_base2lidar * cos(angle);
+  float y_base2lidar = distance_base2lidar * sin(angle);
+
   // Note: The returned values must be set using the `scan` variable:
-  scan.resize(num_ranges);
+  scan.resize(num_ranges);    // Usually 109 scans, 108 + 1
   // Fill in the entries of scan using array writes, e.g. scan[i] = ...
-  for (size_t i = 0; i < scan.size(); ++i) {
-    scan[i] = Vector2f(0, 0);
-  }
+  for (size_t i = 0; i < (scan.size()-1); ++i) {
+    phi = (-135 + 270/(scan.size()-1)*i) * 2*pi()/180;
 
-  // The line segments in the map are stored in the `map_.lines` variable. You
-  // can iterate through them as:
-  for (size_t i = 0; i < map_.lines.size(); ++i) {
-    const line2f map_line = map_.lines[i];
-    // The line2f class has helper functions that will be useful.
-    // You can create a new line segment instance as follows, for :
-    line2f my_line(1, 2, 3, 4); // Line segment from (1,2) to (3.4).
-    // Access the end points using `.p0` and `.p1` members:
-    printf("P0: %f, %f P1: %f,%f\n", 
-           my_line.p0.x(),
-           my_line.p0.y(),
-           my_line.p1.x(),
-           my_line.p1.y());
+    line2f sim_line(loc.x()+x_base2lidar+range_min*cos(phi),
+                    loc.y()+y_base2lidar+range_min*sin(phi),
+                    loc.x()+x_base2lidar+range_max*cos(phi),
+                    loc.y()+y_base2lidar+range_max*sin(phi));
 
-    // Check for intersections:
-    bool intersects = map_line.Intersects(my_line);
-    // You can also simultaneously check for intersection, and return the point
-    // of intersection:
-    Vector2f intersection_point; // Return variable
-    intersects = map_line.Intersection(my_line, &intersection_point);
-    if (intersects) {
-      printf("Intersects at %f,%f\n", 
-             intersection_point.x(),
-             intersection_point.y());
-    } else {
-      printf("No intersection\n");
+    for (size_t n = 0; n < map_.lines.size(); ++n) {
+      const line2f map_line = map_.lines[n];
+
+      Vector2f intersection_point; // Return variable
+      intersects = map_line.Intersection(sim_line, &intersection_point);
+      if (intersects && _Distance(Vector2f (sim_line.p0.x(), sim_line.p0.y(), intersection_point.x(), intersection_point.y())
+                      < _Distance(Vector2f (sim_line.p0.x(), sim_line.p0.y(), scan[i]) {
+        scan[i] = _Distance(Vector2f(sim_line.p0.x(), sim_line.p0.y()), Vector2f(intersection_point.x(), intersection_point.y()));
+      } else {
+        scan[i] = _Distance(Vector2f(sim_line.p0.x(), sim_line.p0.y()), Vector2f(sim_line.p1.x(), sim_line.p1.y()));
+      }
     }
   }
 }
 
-void ParticleFilter::Update(const vector<float>& ranges,
+void ParticleFilter::Update(const vector<float>& ranges, // Laser scans
                             float range_min,
                             float range_max,
                             float angle_min,
@@ -124,6 +122,63 @@ void ParticleFilter::Update(const vector<float>& ranges,
   // observations for each particle, and assign weights to the particles based
   // on the observation likelihood computed by relating the observation to the
   // predicted point cloud.
+
+  // Tunable parameters
+  const float sigma_s = 0.04; // m, Variance of LIDAR, from datasheet
+  float d_short = 1; // Tunable parameter
+  float d_long = 1; // Tunable parameter
+  float gamma = 1; // Tunable parameter
+
+  //Initialize variables
+  vector double log_weights;
+  vector double weights;
+  double log_weights_sum = 0;
+
+  log_weights.resize(ranges.size());
+  weights.resize(log_weights.size());
+
+  for (size_t i = 0; i < log_weights.size(); ++i) {
+    log_weights[i] = ranges.size() * (-log(sigma_s)-0.5*log(2*pi())+gamma);
+  }
+
+  int n = 0;
+  int m = 0;
+  for (const particle_filter::Particle& p: p_ptr) {
+    n = 0;
+
+    vector<Vector2f> predicted_scan;
+    particle_filter_.GetPredictedPointCloud(
+        p_ptr.loc,
+        p_ptr.angle,
+        ranges.size(),
+        range_min,
+        range_max,
+        angle_min,
+        angle_max,
+        &predicted_scan);
+
+    for (auto s: predicted_scan) {
+      if (s < range_min || s > range_max) {
+        // Do nothing
+      } else if (s < range[n] - d_short) {
+        log_weights[i] += -d_short^2/(2*sigma_s^2);
+      } else if (s > range[n] + d_long) {
+        log_weights[i] += -d_long^2/(2*sigma_s^2);
+      } else {
+        log_weights[i] += -(_Distance(s, range[n]))^2/(2*sigma_s^2);
+      } 
+      log_weights_sum += log_weights[i];
+      ++n;
+    }
+
+    for (size_t i = 0; i < log_weights.size(); ++i) {
+      log_weights[i] = log_weights[i]/log_weights_sum;
+      weights[i] = exp(log_weights[i]);
+    }
+
+    p.weight = p.weight * weights[m];
+    ++m;
+  }
 }
 
 void ParticleFilter::Resample() {
